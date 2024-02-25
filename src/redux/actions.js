aceecf773a4c9cef09e40b374f4b81d3024702b6
IhorRatsyn <ratsinjob@gmail.com>
2024-02-25 19:43:40 +0200

    full project

---
+++ src/redux/actions.js
@@ -0,0 +1,38 @@
+import { nanoid } from "nanoid";
+import { useDispatch } from "react-redux";
+
+export const addTask = text => {
+  return {
+    type: "tasks/addTask",
+    payload: {
+      id: nanoid(),
+      completed: false,
+      text,
+    },
+  };
+};
+
+export const deleteTask = taskId => {
+  return {
+    type: "tasks/deleteTask",
+    payload: taskId,
+  };
+};
+
+export const toggleCompleted = taskId => {
+  return {
+    type: "tasks/toggleCompleted",
+    payload: taskId,
+  };
+};
+
+export const setStatusFilter = value => {
+  return {
+    type: "filters/setStatusFilter",
+    payload: value,
+  };
+};
+
+const MyComponent = () => {
+  // Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
